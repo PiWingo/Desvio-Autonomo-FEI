a9f6f30713f0934256c17d86b2e51b42319a26ca
Pietro Montoro Rigetto <pietro.montoro@gmail.com>
2020-10-18 22:31:55 -0300

    Add files via upload

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,69 +1,121 @@
 import cv2
 import tensorflow as ft
 from tensorflow import keras as k
+from keras.models import Sequential,Model
+from keras.layers import Input, Convolution2D, MaxPooling2D, Cropping2D, Conv2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Lambda
 import matplotlib.pyplot as plt
 import numpy as np
 
-f = open(r'C:\Users\pietr\Desktop\DATASETS\DATASET2\commands.txt')
-allLines = f.read().splitlines()
+mapa4 = open(r'C:\Users\pietr\Desktop\DATASETS\mapa4\commands.txt')
+mapa7 = open(r'C:\Users\pietr\Desktop\DATASETS\mapa7\commands.txt')
+allLines4 = mapa4.read().splitlines()
+allLines7 = mapa7.read().splitlines()
 inputArray = []
 outputArray = []
+mapa4.close()
+mapa7.close()
 
 print('Loading dataset...')
 
-for line in allLines:
-
-	imgL = None
+for line in allLines4:
 	imgR = None
-
 	params = line.split(';')
-	
 	timestamp = params[0]
-	acceleration = params[1]
 	steering = params[2]
-	breakValue = params[3]
-
 	try:
-		imgR = cv2.imread(r"C:\Users\pietr\Desktop\DATASETS\DATASET2\right\right-"+ timestamp +".jpg", cv2.IMREAD_GRAYSCALE)
-		imgDepth = cv2.imread(r"C:\Users\pietr\Desktop\DATASETS\DATASET2\depth\depth-" + timestamp +".jpg", cv2.IMREAD_GRAYSCALE)
+		imgR = cv2.imread(r"C:\Users\pietr\Desktop\DATASETS\mapa4\right\right-"+ timestamp +".jpg", cv2.IMREAD_GRAYSCALE)
+		imgDepth = cv2.imread(r"C:\Users\pietr\Desktop\DATASETS\mapa4\depth\depth-" + timestamp +".jpg", cv2.IMREAD_GRAYSCALE)
 
 		imgArray = np.array([imgR, imgDepth])
 		imgArray = np.moveaxis(imgArray, 0, 2)
 
 		inputArray.append(imgArray)
-		outputArray.append(np.array([float(acceleration), float(steering), float(breakValue)]))
+		outputArray.append(np.array([float(steering)]))
 
 	except:
-		print('error loading: ' + timestamp)
+		print('mapa4 error loading: ' + timestamp)
+
+print('map4 input size: ' + str(len(inputArray)))
+
+for line in allLines7:
+	imgR = None
+	params = line.split(';')
+	timestamp = params[0]
+	steering = params[2]
+	try:
+		imgR = cv2.imread(r"C:\Users\pietr\Desktop\DATASETS\mapa7\right\right-"+ timestamp +".jpg", cv2.IMREAD_GRAYSCALE)
+		imgDepth = cv2.imread(r"C:\Users\pietr\Desktop\DATASETS\mapa7\depth\depth-" + timestamp +".jpg", cv2.IMREAD_GRAYSCALE)
+
+		imgArray = np.array([imgR, imgDepth])
+		imgArray = np.moveaxis(imgArray, 0, 2)
+
+		inputArray.append(imgArray)
+		outputArray.append(np.array([float(steering)]))
+
+	except:
+		print('mapa7 error loading: ' + timestamp)
 
 print('Dataset loaded')
 
 print('Validating dataset')
 
-for i in range(0,len(inputArray)):
-	print('index number ' + str(i))
-	print('index shape ' + str(inputArray[i].shape))
-	# print('index output value ' + str(outputArray[i]))
-
-
 print('inputArray size: ' + str(len(inputArray)))
 print('outputArray size: ' + str(len(outputArray)))
-print(inputArray[0].shape)
-print(outputArray[0])
+
+print('Test' + str(np.array(inputArray[0]).shape))
 
 print('Executing model instructions...')
-inp = k.layers.Input([480,640,2])
-l1 = k.layers.Convolution2D(4, 7, 7, data_format='channels_last')(inp)
-l2 = k.layers.Flatten()(l1)
-l3 = k.layers.Dense(3)(l2)
-model = k.Model(inp, l3)
+
+
+#model
+model = Sequential()
+
+#normalizing and cropping 
+model.add(Lambda(lambda x:x/255.0 - 0.5, input_shape=(480,640,2)))
+# model.add(Cropping2D(cropping=((206,72), (0,0)))) # 43%, 15%
+
+#layer 1
+model.add(Conv2D(32, (7,7), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(rate=0.3))
+
+#layer 2
+model.add(Conv2D(32, (7,7), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(rate=0.3))
+
+model.add(Flatten())
+
+#fc 1
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(rate=0.3))
+#fc 2
+model.add(Dense(32, activation='relu'))
+#fc 3
+model.add(Dense(1))
+
 model.summary()
-model.compile(loss=k.losses.MeanSquaredError())
-print('Model instructions finished')
-print('Starting training...')
-print(np.array(inputArray))
+
+model.compile(loss='mse', optimizer='adam')
+print(np.array(inputArray).shape)
+print(np.array(outputArray).shape)
 model.fit(np.array(inputArray), np.array(outputArray)) # a,s,b
-print('Finished training...')
-print('Exporting model...')
-model.save('seguidor.h5')
-print('Script finished')
+# model.save('seguidor2.h5')
+
+# inp = k.layers.Input([480,640,2])
+# l1 = k.layers.Convolution2D(4, 7, 7, data_format='channels_last')(inp)
+# l2 = k.layers.Flatten()(l1)
+# l3 = k.layers.Dense(3)(l2)
+# model = k.Model(inp, l3)
+# model.summary()
+# model.compile(loss=k.losses.MeanSquaredError())
+# print('Model instructions finished')
+
+# print('Starting training...')
+# # model.fit(np.array(inputArray), np.array(outputArray)) # a,s,b
+# print('Finished training')
+
+# print('Exporting model...')
+# model.save('seguidor.h5')
+# print('Script finished')
